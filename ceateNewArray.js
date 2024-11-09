7f80e0fe70de53315ce5ba27405d4ad67f97b4b0
kiran kumhar <kiran.kumar91@live.com>
2024-11-09 11:45:45 +0530

    map mathod practice

---
+++ ceateNewArray.js
@@ -0,0 +1,8 @@
+//? Create a New Array : Use map() to create a new array where each numbers in the array of the numbers is doubled
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+let doubleNumber = numbers.map((num) => num * 2);
+
+console.log(doubleNumber);
