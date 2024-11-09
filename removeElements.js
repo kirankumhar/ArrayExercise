0ff3ef0e559c5f95db5e96f41a24b5253640e842
kiran kumhar <kiran.kumar91@live.com>
2024-11-09 11:46:25 +0530

    pop and shift methods

---
+++ removeElements.js
@@ -0,0 +1,8 @@
+//? Remove Elements: Use pop() and shift() to remove colors from the array.
+
+let colors = ['red', 'black', 'blue', 'green'];
+
+// colors.pop(); //pop method remove elements form last 
+colors.shift();  //shift method remove element form first 
+
+console.log(colors);
