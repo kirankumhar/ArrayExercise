bf96c6b18a4b8f1b484e6783bdaec60ee4651677
kiran kumhar <kiran.kumar91@live.com>
2024-11-09 11:31:21 +0530

    adding elements methods

---
+++ addElements.js
@@ -0,0 +1,9 @@
+//? Add Elements: Create an array of colors and use push() and unshift() to add new colors to the array
+
+
+let colors = ['red', 'black','blue'];
+
+// colors.push('yellow'); // push() method add element in last 
+colors.unshift('orange'); // unshift() method add elements in first 
+
+console.log(colors);
