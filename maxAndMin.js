f58080ed014a8c82e1fbf79bdf7571a3a183c32d
kiran kumhar <kiran.kumar91@live.com>
2024-11-16 10:13:13 +0530

    maximum and minimum finder

---
+++ maxAndMin.js
@@ -0,0 +1,8 @@
+let numbers = [12, 34, 65, 32,33, 46, 5, 46, 56, 4, 6, 305, 465, 405, 46, 787, 46, 4, 65, 46, 540, 60, 46, 46, 460, 98, 46, 404, 65, 700, 402, 134, 65];
+
+let maxNum  = Math.max(...numbers);
+
+let minNum = Math.min(...numbers);
+
+console.log(maxNum);
+console.log(minNum);
